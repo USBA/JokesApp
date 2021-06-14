ae2baf24dec261e94e1d316d7e1cea04fdb9ff5a
Umayanga Alahakoon <umayanga@usa.com>
2021-06-14 19:09:23 +0530

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Umayanga Alahakoon on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
